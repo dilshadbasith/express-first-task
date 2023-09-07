9b5cec1f657a688526014979b56b515a88213e5f
dilshad <dilshadbasith01@gmail.com>
2023-09-07 12:25:27 +0530

    first commit

---
+++ userinfo.js
@@ -0,0 +1,11 @@
+module.exports=[{
+    id:1,
+    username:"dilu@12",
+    name:"dilshad",
+    email:"dilu@gmail.com"
+},{
+    id:2,
+    username:"dilu@12",
+    name:"dilshad",
+    email:"dilu@gmail.com"
+}]
